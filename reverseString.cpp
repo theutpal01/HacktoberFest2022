2edb097e96f449136686eb3ab6c40abd3dc92126
Aryan <aryan4p130202@gmail.com>
2022-10-01 02:14:05 +0530

    Added new solution

---
+++ reverseString.cpp
@@ -0,0 +1,9 @@
+// Reverse String Entered by User - C++
+// CREATED BY - Aryan
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s; cin>>s;
+    reverse(s.begin(), s.end());
+    cout<<s<<endl;
+}
