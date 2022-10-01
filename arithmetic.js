76ccc31f30c384a0d9e111802afcbb0275571bf0
Rajendra Rakha <1941720080@student.polinema.ac.id>
2022-10-01 21:41:24 +0700

    Create arithmetic.js

---
+++ arithmetic.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>JavaScript Arithmetic</h1>
+<h2>The + Operator</h2>
+
+<p id="demo"></p>
+
+<script>
+let x = 5;
+let y = 2;
+let z = x + y;
+document.getElementById("demo").innerHTML = z;
+</script>
+
+</body>
+</html>
