49de44454e92ee954f5a196ae4cae0c5489ccc1a
parv07web <114813280+parv07web@users.noreply.github.com>
2022-10-01 18:11:43 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,13 +1,9 @@
-#include <iostream>  
-using namespace std;  
-int main()  
-{  
-   int i,fact=1,number;    
-  cout<<"Enter any Number: ";    
- cin>>number;    
-  for(i=1;i<=number;i++){    
-      fact=fact*i;    
-  }    
-  cout<<"Factorial of " <<number<<" is: "<<fact<<endl;  
-  return 0;  
-}  
+#include <iostream>
+using namespace std;
+int main() {
+   int n = 5, fact = 1, i;
+   for(i=1; i<=n; i++)
+   fact = fact * i;
+   cout<<"Factorial of "<<n<<" is "<<fact;
+   return 0;
+}
