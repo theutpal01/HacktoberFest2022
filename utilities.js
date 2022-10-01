d71987ad6e9b3a5a9fa905cb8f1e5f1ec9ba59bc
Akshat Mittal <akshatmittal2506@gmail.com>
2022-10-01 22:42:07 +0530

    intialized uilities.js

---
+++ utilities.js
@@ -0,0 +1,6 @@
+// This is a collection of some useful functions in JavaScript, which have proved them useful to many of my projects
+
+// Max and Min
+
+export const max = (a, b) => (a > b ? a : b);
+export const min = (a, b) => (a < b ? a : b);
