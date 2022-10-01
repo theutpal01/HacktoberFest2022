b09bc7bfb5a0dc8f60216e2d42e229aa223b741e
Rajendra Rakha <1941720080@student.polinema.ac.id>
2022-10-01 21:47:16 +0700

    Create numberproperties.js

---
+++ numberproperties.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Number Properties</h2>
+
+<p>POSITIVE_INFINITY is returned on overflow:</p>
+
+<p id="demo"></p>
+
+<script>
+let x = 1 / 0;
+document.getElementById("demo").innerHTML = x;
+</script>
+
+</body>
+</html>
