82e877e19b8cfd61d7b96be56cbeabb5fdad263d
Aakash Jha <72294865+sky-jha@users.noreply.github.com>
2022-10-02 17:48:30 +0530

    SecondLargestInArray.java

---
+++ SecondLargestInArray.java
@@ -0,0 +1,14 @@
+import java.util.*;  
+public class SecondLargestInArray{  
+public static int getSecondLargest(Integer[] a, int total){  
+List<Integer> list=Arrays.asList(a);  
+Collections.sort(list);  
+int element=list.get(total-2);  
+return element;  
+}  
+public static void main(String args[]){  
+Integer a[]={1,2,5,6,3,2};  
+Integer b[]={44,66,99,77,33,22,55};  
+System.out.println("Second Largest: "+getSecondLargest(a,6));  
+System.out.println("Second Largest: "+getSecondLargest(b,7));  
+}}  
