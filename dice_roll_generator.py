97ac5d37d398faf539e7f9a355da4021b95fd575
Roopika Ponnur <prkroopika253@gmail.com>
2022-10-02 10:14:21 +0530

    Added Dice roll generator in python

---
+++ dice_roll_generator.py
@@ -0,0 +1,5 @@
+#This program returns a random integer in the range of 1 to 6
+import random
+#randint() method returns a random number between the given range
+random_number=(random.randint(1,6))
+print("Random Number on the dice:",random_number)
