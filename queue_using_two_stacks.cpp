3358da1ae8dd12bb873e69b7de6c55fcb0bc82c9
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-10-06 01:54:12 +0530

    Queue using two Stacks

---
+++ queue_using_two_stacks.cpp
@@ -0,0 +1,91 @@
+//Queue using two Stacks
+//https://practice.geeksforgeeks.org/problems/queue-using-two-stacks/1#
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class StackQueue{
+private:
+    stack<int> s1;
+    stack<int> s2;
+public:
+    void push(int B);
+    int pop();
+
+};
+int main()
+{
+
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        StackQueue *sq = new StackQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+
+
+}
+// } Driver Code Ends
+
+
+/* The structure of the class is
+class StackQueue{
+private:   
+    // These are STL stacks ( http://goo.gl/LxlRZQ )
+    stack<int> s1;
+    stack<int> s2;
+public:
+    void push(int);
+    int pop();
+}; */
+
+//Function to push an element in queue by using 2 stacks.
+void StackQueue :: push(int x)
+{
+    // Your Code
+    while(!s1.empty()){
+        s2.push(s1.top());
+        s1.pop();
+    }
+    
+    s1.push(x);
+    
+    while(!s2.empty()){
+        s1.push(s2.top());
+        s2.pop();
+    }
+}
+
+//Function to pop an element from queue by using 2 stacks.
+int StackQueue :: pop()
+{
+        // Your Code
+        int popped_ele;
+        if(!s1.empty()){
+            popped_ele=s1.top();
+            s1.pop();
+        }
+        else
+            popped_ele=-1;
+            
+        return popped_ele;
+}
