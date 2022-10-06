f5c183e31d427f8580637e723f9bce8703b29f12
sakshamsharma26 <sakshamsai26@gmail.com>
2022-10-07 00:07:08 +0530

    Modified adding.py

--- adding.py
+++ adding.py
@@ -1,18 +1,4 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-
-  int first_number, second_number, sum;
-    
-  cout << "Enter two integers: ";
-  cin >> first_number >> second_number;
-
-  // sum of two numbers in stored in variable sumOfTwoNumbers
-  sum = first_number + second_number;
-
-  // prints sum 
-  cout << first_number << " + " <<  second_number << " = " << sum;     
-
-  return 0;
-}
+first_number=int(input("Enter First Number")) 
+second_number=int(input("Enter Second Number"))
+sum=first_number+second_number
+print(sum)
