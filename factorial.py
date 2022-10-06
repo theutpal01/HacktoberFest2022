4318b81072dee9bf27327b17f8fe913efe5192ac
sakshamsharma26 <106482027+sakshamsharma26@users.noreply.github.com>
2022-10-06 23:22:40 +0530

    Modified factorial.py

--- factorial.py
+++ factorial.py
@@ -1,10 +1,7 @@
-n = int(input("Enter your number: "))    
-factorial = 1    
-if n < 0:    
-   print(" Factorial doesn't exist for negative numbers !!")    
-elif n == 0:    
-   print("Factorial of 0 = 1")    
-else:    
-   for i in range(1,n + 1):    
-       factorial = factorial * i    
-   print("Factorial of",n,"is :",factorial)
+def f(n):
+    if n==0:
+        return 1
+    return n* f(n-1)
+n= int(input("Enter the Number you wanna Find Factorial Of :"))
+if(n<0): print("Factorial of negative numbers doesn't exist")
+else: print(f(n))
