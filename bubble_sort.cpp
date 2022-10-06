57b6a098e6c3c8ba043fe65cca2dbd35c9f4fe1c
Karan Singh <92674689+karansingh1820@users.noreply.github.com>
2022-10-06 19:57:58 +0530

    Update(Bubble sort)
    This is the program of optimized Bubble sort by by using the concept of pointers(used in swap functions),function created and calling.Previous code i also correct
    and i have comment down that and some changes in it.

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,38 +1,88 @@
+/* Optimized BUBBLE SORT */
+#include<bits/stdc++.h>
 #include<iostream>
 #include<string>
 using namespace std;
-int main(){
-    int arr1[] = {1,2,3,98,5,6};
-    bool swap=false;
-    for(int i=0;i<6;i++){
-        for(int j=0;j-i;j++){
-            int temp;
+void swap(int *a,int *b)
+{
+    int c=*a;
+    *a=*b;
+    *b=c;
+}
+void bubble_sort(int a[],int n)
+{
+    int i,j;
+    bool temp;
+    for(i=0;i<n-1;i++)
+    {
+        temp=false;
+       for(j=0;j<n-1-i;j++)
+       {
+           if(a[j]>a[j+1])
+           {
+               swap(&a[j],&a[j+1]);
+               temp=true;
+           }
+       }
+       if(temp==false)
+       {
+           break;
+       }
+    }
+
+}
+void print(int a[],int n)
+{
+    int t;
+    for(t=0;t<n;t++)
+    {
+       cout<<a[t]<<" "; 
+    }
+    cout<<"\n";
+}
+int main()
+{
+    int n;
+cout<<"Enter the size of array:";
+cin>>n;
+int a[n];
+cout<<"\nEnter the elements of array:";
+for(int i=0;i<n;i++)
+{
+    cin>>a[i];
+}
+bubble_sort(a,n);
+cout<<"\nSorted array: ";
+print(a,n);
+return 0;
+}
+
+// {
+//     int arr1[] = {1,2,3,98,5,6};
+//     bool swap=false;
+//     for(int i=0;i<6;i++){
+//         for(int j=0;j-i;j++){
+//             int temp;
             
-                if((arr1[j]>arr1[j+1])&&swap==false){
-                    temp = arr1[j];
-                    arr1[j]=arr1[j+1];
-                    arr1[j+1]=temp;
-                    swap = true;
+//                 if((arr1[j]>arr1[j+1])&&swap==false){
+//                     temp = arr1[j];
+//                     arr1[j]=arr1[j+1];
+//                     arr1[j+1]=temp;
+//                     swap = true;
                     
 
             
 
-            }
+//             }
             
-        }
-        swap = false;
-    }
+//         }
+//         swap = false;
+//     }
 
-    for(int i = 0; i < 6; i++)
-{
-    cout<<arr1[i];
-}
-
-
-
-
-    
-
-}
+//     for(int i = 0; i < 6; i++)
+// {
+//     cout<<arr1[i];
+// }
+// }
 
 
