37bfe40363db39e9b69a9059f5a3b8d78237ffb7
handsomeharsh <91953788+handsomeharsh@users.noreply.github.com>
2022-10-07 23:50:33 +0530

    Create shuffle cards.py

---
+++ shuffle cards.py
@@ -0,0 +1,13 @@
+# importing modules
+import itertools, random
+
+# make a deck of cards
+deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
+
+ # shuffle the cards
+ random.shuffle(deck)
+
+ # draw five cards
+ print("You Got:")
+ for i in range(5):
+    print(deck[i][0], "of", deck[i][1]
