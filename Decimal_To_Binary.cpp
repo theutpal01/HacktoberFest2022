6470f98745df5b05303a6625161275198bba915c
Srishty Mangutte <srishtymangutte@gmail.com>
2022-10-11 16:24:29 +0530

    Update Decimal_To_Binary.cpp

--- Decimal_To_Binary.cpp
+++ Decimal_To_Binary.cpp
@@ -6,5 +6,14 @@ int main(){
     cout<<"Enter a Decimal value:";
     int a;
     cin>>a;
-    cout<<"The value in Binary would be :"<<bitset<8>(a).to_string()<<endl;
+    int bin=0;
+    int a2=a;
+    while(a2){
+        int rem=a2%2;
+        bin+=rem;
+        bin*=10;
+        a2/=2;
+    }
+
+    cout<<a<<" in binary is "<<bin ;
 }
