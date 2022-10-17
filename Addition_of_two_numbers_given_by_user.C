1302266e60e3180ca589e6b6fb51bbb5080e9d8e
BitanKundu1 <115944988+BitanKundu1@users.noreply.github.com>
2022-10-17 13:12:43 +0530

    Create Addition_of_two_numbers_given_by_user.C
    C programming language code

---
+++ Addition_of_two_numbers_given_by_user.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  int a,b;
+  printf("The value of a is: ");
+  scanf("%d",&a);
+  printf("The value of b is: ");
+  scanf("%d",&b);
+  printf("The sum of two numbers are: %d",a+b);
+  return 0;
+}
