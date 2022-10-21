576ae9b952f8e0dbc537673900d92fb7f1962a25
harshsavani <89725795+harshsavani@users.noreply.github.com>
2022-10-21 11:41:08 +0530

    Update Add_two_number.py

--- Add_two_number.py
+++ Add_two_number.py
@@ -1,3 +1,9 @@
-number1=int(input("Enter First  Number"))
-number2=int(input("Enter Second Number"))
-print(number1+number2)
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
