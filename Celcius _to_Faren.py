c408b449245d05cd69e22da9bc3ac441fb034b8b
Rosotron <binitb11@gmail.com>
2022-10-21 12:50:35 +0530

    fixed spelling errors

--- Celcius _to_Faren.py
+++ Celcius _to_Faren.py
@@ -1,14 +1,14 @@
-print('To convert CELCIUS to FARENHEIT press "1" \nTo convert FARENHEIT to CELCIUS press "2"')
+print('To convert CELSIUS to FAHRENHEIT press "1" \nTo convert FAHRENHEIT to CELSIUS press "2"')
 num = input('Enter number for selection :')
-val = input("Enter the vslue to be converted:")
+val = input("Enter the value to be converted:")
 
 if num == '1':
     num1 = (float(val)*9/5)+32
-    print('CELCIUS to FARENHEIT is : ',num1)
+    print('CELSIUS to FAHRENHEIT is : ',num1)
 
 elif num == '2':
     num1 = (float(val)-32)*5/9
-    print('FARENHEIT to CELCIUS : ',num1)
+    print('FAHRENHEIT to CELSIUS : ',num1)
 
 else:
     print('Unkown Command!!!! /n Exiting!!!')
