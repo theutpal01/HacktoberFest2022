4daa9e38370323f420c2eecae54c8cc685cf6b88
Vineet Kumar <vkrmgs69@gmsil.com>
2022-10-26 16:16:33 +0530

    Another code for calculator

--- calculateLcm.py
+++ calculateLcm.py
@@ -1,24 +1,19 @@
-# A simple program to calculate LCM and GCD from input numbers
-# By: Anish Shilpakar
-# GitHub Username: juju2181
-
-def findGCD(x, y):
-    while x != y:
-        if x > y:
-            x = x - y
-        elif x < y:
-            y = y - x
-    return x
-
-
-def findLCM(x, y):
-    z = x*y
-    gcd = findGCD(x, y)
-    lcm = int(z/gcd)
-    return lcm
-
-
-x = int(input("n1: "))
-y = int(input("n2: "))
-print(f'GCD of {x} and {y} : {findGCD(x,y)}')
-print(f'LCM of {x} and {y} : {findLCM(x,y)}')
+# store input number
+num1 = input('enter first number:')
+num2 = input('enter second number:')
+# Add two numbers
+sum: float = float(num1) + float(num2)
+# Subtract two numbers
+min: float = float(num1) - float(num2)
+# Multiplying two numbers
+mul = float(num1) * float(num2)
+# Dividing two numbers
+div = float(num1) / float(num2)
+# Display the addition
+print('the sum of {0}and {1} is {2}'.format(num1, num2, sum))
+# Display the subtraction
+print('the subtraction of {0} and {1} is {2}'.format(num1, num2, min))
+# Display the multiplication
+print('the mul of {0}and {1} is {2}'.format(num1, num2, mul))
+# Display the division
+print('the div of {0}and {1} is {2}'.format(num1, num2, div))
